fb7a74dee07c08c5c51d8b788559b5eb2a455c7e
ashrafbd1496 <asraf1496@gmail.com>
2023-08-01 11:08:01 +0600

    added word count using add filter

--- functions.php
+++ functions.php
@@ -1,6 +1,18 @@
 <?php 
 
+
 function my_des_filter_func($val){
-    return $val;
+
+    $wordnumber = str_word_count($val);
+
+    return $val . '
+    <hr>
+    <input name="review" type="radio" id="g"><label for="g"> Good </label>
+    <input name="review" type="radio" id="b"><label for="b"> Bad </label> 
+    <hr>
+    <hr>
+  Total Word Number: ' . $wordnumber ;
+   
 }
 add_filter('my_desc',"my_des_filter_func");
+
--- index.php
+++ index.php
@@ -11,7 +11,7 @@
    
    echo apply_filters('my_name', "<h1>Ashraf Uddin</h1>");
    
-   echo apply_filters("my_desc","<p>I'm a seasoned WordPress expert with over 5 years of experience in web development.Worked on a diverse range of projects, from blogs to WooCommerce and Shopify websites, and have completed 3 Laravel projects.</p>"); 
+   echo apply_filters("my_desc","<p>I'm a seasoned WordPress expert with over 5 years of experience in web development.Worked on a diverse range of projects, from blogs to WooCommerce and Shopify websites, and have completed 3 Laravel projects.My passion for learning and my ability to blend technical expertise with a deep understanding of user experience make me a reliable and forward-thinking developer, consistently delivering exceptional results and making online dreams come to life. </p>"); 
    
   
   
